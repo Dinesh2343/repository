58aead1316bdb53c926f8116ec3fd151e1619575
Dinesh2343 <dinesheee673@gmail.com>
2021-08-10 18:14:12 +0530

    A

--- src/org/emp/Sample.java
+++ src/org/emp/Sample.java
@@ -1,6 +1,19 @@
 package org.emp;
 
 public class Sample extends Employee{
+	
+	private void type() {
+System.out.println("type");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 	Sample s = new Sample();
